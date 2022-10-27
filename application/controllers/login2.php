d36ce19c3a2f79cdc352d36a6e595c1a2ebe57b0
Ariel1100 <84751681+Ariel1100@users.noreply.github.com>
2022-10-27 01:15:36 -0400

    se agrega nuevo login

---
+++ application/controllers/login2.php
@@ -0,0 +1,112 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		// Comunicacion con el modelo
+		$this->load->model("usuario_model");
+	}
+
+
+	public function index()
+	{
+		$data['msg']=$this->uri->segment(3);
+		if ($this->session->userdata('login')) 
+		{
+			//redirect('usuario','refresh'); // aqui entraba panel (login/panel)
+			$this->load->view('login/panel','refresh');
+		}
+		else
+		{
+			$this->load->view('login/index',$data);
+		}
+
+	}
+
+	public function validarusuario()
+	{
+		$usuario=$_POST['txtLogin'];
+		$clave=md5($_POST['txtClave']);
+		//$clave=$_POST['txtClave'];
+		
+		$consulta=$this->usuario_model->validarUsuario($usuario,$clave);
+		if ($consulta->num_rows()>0) 
+		{
+			foreach ($consulta->result() as $row) 
+			{
+				$this->session->set_userdata('idUsuario',$row->idUsuario);
+				$this->session->set_userdata('login',$row->login);
+				$this->session->set_userdata('tipo',$row->tipo);
+				$this->session->set_userdata('foto',$row->foto);
+				$this->session->set_userdata('nombres',$row->nombres);
+				$this->session->set_userdata('pA',$row->primerApellido);
+				redirect('login/panel','refresh');// sesion exitosa
+				
+				//redirect('usuario','refresh');
+			}
+		}
+		else
+		{
+			redirect('login/index/1','refresh'); // del ing Pavel
+			//redirect('login','refresh');
+			header("location: login?fallo=true");
+			//$data['msg']="Error de entrada";
+			//$this->load->view('login/login',$data);
+		}
+
+	}
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('login/index/2','refresh');
+		//redirect('login/index/3','refresh');
+
+	}
+
+	
+	public function panel()
+	{
+		/*if ($this->session->userdata('usuario')) 
+		{
+			$this->load->view('inic/header'); 
+			$this->load->view('tablero/index');
+			$this->load->view('inic/footer');
+			
+		}
+		else
+		{
+			//$this->load->view('loginform',$data);
+			//redirect('login/index/2','refresh');
+			redirect('login','refresh');
+		}*/
+
+		if ($this->session->userdata('login')) 
+		{
+			if($this->session->userdata('tipo')=='1'){
+				redirect('tablero/index','refresh');
+			} else{
+				redirect('producto/index','refresh');
+			}
+			
+		}
+		else
+		{
+			//$this->load->view('loginform',$data);
+			//redirect('login/index/2','refresh');
+			redirect('login/index/3','refresh');
+		}
+
+	}
+
+
+
+
+
+
+
+
+}
+
